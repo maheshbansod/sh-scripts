fd17fbf0ff35ed8c12914aacdd69829f2288d97a
Mahesh Bansod <coolkevin011@gmail.com>
2023-08-06 01:28:20 +0530

    add projects searcher

---
+++ procs.sh
@@ -0,0 +1,12 @@
+# Show a dmenu for directories inside the projects directory
+p() {
+	projects_dir=projects
+	red="#FF0000"
+	green="#00FF00"
+	lines=5
+	prompt="Project >"
+	dirname=$(find ~/$projects_dir -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | dmenu -b -p "$prompt" --nf "$green" --sb "$red" -l $lines --font "FiraCode-21")
+	if [ ! -z "$dirname" ]; then
+		cd ~/$projects_dir/$dirname
+	fi
+}
