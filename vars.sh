49f4141c9a6c4d1faf2f7d5f9f135afc0d7f5ac2
Mahesh Bansod <coolkevin011@gmail.com>
2023-08-15 17:22:59 +0530

    add git branch in prompt

--- vars.sh
+++ vars.sh
@@ -1,6 +1,12 @@
 
 export PATH=~/bin:$PATH
 # TODO: add git branch in prompt
-export PS1=" \e[34;1m\W\e[0m\n \$ "
+git_branch_formatted() {
+	local current_branch=$(git_current_branch 2> /dev/null)
+	if [[ ! -z "$current_branch" ]]; then
+		echo -e "\uE0A0 $current_branch"
+	fi
+}
+export PS1=" \e[34;1m\W\e[0m \$(git_branch_formatted)\n \$ "
 
 export HISTCONTROL=ignoreboth
