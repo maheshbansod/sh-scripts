d872be282ae67484d9a943635389ad4a75e30ec5
Mahesh Bansod <coolkevin011@gmail.com>
2023-08-15 17:26:46 +0530

    add gpsup alias

--- aliases.sh
+++ aliases.sh
@@ -13,6 +13,7 @@ alias gc="git commit"
 alias gd="git diff"
 alias gsw="git switch"
 alias gswc="git switch -c"
+alias gpsup="git push -u origin $(git_current_branch)"
 
 alias grep=rg
 alias cat=bat
